d9b1df406b5983eb9b0c8a65238a00f82555dcb6
GRND0 <zefiro.turchetta@gmail.com>
2022-05-09 17:38:47 +0200

    terminato codice prime due milestone

--- script.js
+++ script.js
@@ -2,7 +2,31 @@ const app = new Vue (
     {
         el:'#root',
         data: {
-            
+            dafare: [
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Prenotare visita',
+                    done: true
+                },
+                {
+                    text: 'Pagare bolletta',
+                    done: true
+                },
+                {
+                    text: 'Comprare pellicola',
+                    done: false
+                },
+            ],
+        },
+
+        methods: { 
+            rimuovi: function(index) {
+                this.dafare.splice(index,1);
+            },
         }
 
 })
+
